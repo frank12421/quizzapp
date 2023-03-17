b9523201e07d0cfcdfa17de2a22230804caa74c1
Frank Böker <boeker.frank@gmail.com>
2023-03-17 04:59:03 +0100

    add js click-toggle to Bookmark Button

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-const buttonAnswer = document.querySelector('[data-js="button-answer"]');
+const buttonAnswer = document.querySelector('[data-js="buttonAnswer"]');
+buttonAnswer.addEventListener("click", () => {
+  console.log("Click an Button Show Answer");
+});
+
+// ---- Card Bookmark click Start ------
+const bookmarkButtonCard = document.querySelector(
+  '[data-js="bookmarkButtonCard"]'
+);
+const bookmarkButtonSvg = document.querySelector(
+  '[data-js="bookmarkButtonSvg"]'
+);
+
+bookmarkButtonCard.addEventListener("click", () => {
+  bookmarkButtonSvg.classList.toggle("bookmark__button__svg--marked");
+});
+// ---- Card Bookmark click End ------
